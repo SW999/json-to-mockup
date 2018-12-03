8b2a26e83addfbc9998aa4e3603a7e90252a66fa
Siarhei Vaitehovich <swarog2002@mail.ru>
2018-12-03 14:37:23 +0300

    Updates

---
+++ precache-manifest.897c6095d6da50485e50fc6b38854e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66254b3d0bb8f96d9f5e9bf26410b53",
+    "url": "/projects/json-to-mockup/static/media/broken_heart.b66254b3.svg"
+  },
+  {
+    "revision": "2b987a71f94692c18cb2",
+    "url": "/projects/json-to-mockup/static/js/runtime~main.2b987a71.js"
+  },
+  {
+    "revision": "3bb7faeed30d9ca7c944",
+    "url": "/projects/json-to-mockup/static/js/main.3bb7faee.chunk.js"
+  },
+  {
+    "revision": "e0587f3714af5528d23a",
+    "url": "/projects/json-to-mockup/static/js/1.e0587f37.chunk.js"
+  },
+  {
+    "revision": "3bb7faeed30d9ca7c944",
+    "url": "/projects/json-to-mockup/static/css/main.27714f9e.chunk.css"
+  },
+  {
+    "revision": "fc400c2f25689bba6532f3c64e2e13e5",
+    "url": "/projects/json-to-mockup/index.html"
+  }
+];
